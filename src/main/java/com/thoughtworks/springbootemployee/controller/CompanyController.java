c0596a9c7c8b056f97d9e8fac7add8b30ac8023e
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-21 21:55:02 +0800

    [Employee Controller] [alfred & baron] test: add test cases to cover cases provided for company

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -20,5 +21,30 @@ public class CompanyController {
     public Company createCompany(@RequestBody Company company) {
         companies.add(company);
         return company;
+    }
+
+    @GetMapping(path = "/{id}")
+    public Company getCompanyById(@PathVariable Integer id) {
+        return null;
+    }
+
+    @GetMapping(path = "/{id}/employees")
+    public List<Employee> getAllEmployeesUnderCompany(@PathVariable Integer id) {
+        return null;
+    }
+
+    @GetMapping(params = {"page", "pageSize"})
+    public List<Company> getByPage(@RequestParam("page") int page, @RequestParam("pageSize") int pageSize) {
+        return null;
+    }
+
+    @PutMapping(path = "/{id}")
+    public Company updateCompanyById(@PathVariable Integer id, @RequestBody Company updatedCompany) {
+        return null;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deleteCompanyById(@PathVariable Integer id) {
+        // TODO
     }
 }

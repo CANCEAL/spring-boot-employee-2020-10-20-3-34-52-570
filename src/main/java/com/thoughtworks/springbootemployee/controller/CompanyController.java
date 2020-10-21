c6178f9301eea8351ba95b342757245b84f02b14
CANCEAL <alfred.cancejo@oocl.com>
2020-10-21 23:30:32 +0800

    [Employee Controller] [Alfred & Baron] Feature: implemented CompanyService on CompanyController

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -2,61 +2,53 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.service.CompanyService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
-    List<Company> companies = new ArrayList<>();
+    CompanyService companyService;
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
 
     @GetMapping
     public List<Company> getCompanies() {
-        return companies;
+        return companyService.getAll();
     }
 
     @PostMapping
     public Company createCompany(@RequestBody Company company) {
-        companies.add(company);
-        return company;
+        return companyService.create(company);
     }
 
     @GetMapping(path = "/{code}")
     public Company getCompanyByCode(@PathVariable Integer code) {
-        return companies.stream()
-                .filter(company -> company.getCode().equals(code))
-                .findFirst()
-                .orElse(null);
+        return companyService.retrieve(code);
     }
 
-    @GetMapping(path = "/{id}/employees")
+    @GetMapping(path = "/{id}")
     public List<Employee> getAllEmployeesUnderCompany(@PathVariable Integer id) {
         return null;
     }
 
     @GetMapping(params = {"page", "pageSize"})
     public List<Company> getByPage(@RequestParam("page") int page, @RequestParam("pageSize") int pageSize) {
-        return companies.stream()
-                .skip(page * pageSize)
-                .limit(pageSize)
-                .collect(Collectors.toList());
+        return companyService.getByPage(page, pageSize);
     }
 
     @PutMapping(path = "/{code}")
     public Company updateCompanyByCode(@PathVariable Integer code, @RequestBody Company updatedCompany) {
-        companies.remove(getCompanyByCode(code));
-        companies.add(updatedCompany);
+        companyService.update(code, updatedCompany);
         return updatedCompany;
     }
 
     @DeleteMapping(path = "/{code}")
     public void deleteCompanyById(@PathVariable Integer code) {
-        companies.stream()
-                .filter(company -> company.getCode().equals(code))
-                .findFirst()
-                .ifPresent(company -> companies.remove(company));
+        companyService.delete(code);
     }
 }

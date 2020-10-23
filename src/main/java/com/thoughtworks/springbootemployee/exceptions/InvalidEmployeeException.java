c9820581fc4856e0836184d3ecac14e3954b0615
CANCEAL <alfred.cancejo@oocl.com>
2020-10-23 13:24:57 +0800

    [Employee Controller] [Alfred & Leo] Feature: created InvalidEmployeeException class

---
+++ src/main/java/com/thoughtworks/springbootemployee/exceptions/InvalidEmployeeException.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.springbootemployee.exceptions;
+
+public class InvalidEmployeeException extends RuntimeException {
+
+    public InvalidEmployeeException(String message) {
+        super(message);
+    }
+}

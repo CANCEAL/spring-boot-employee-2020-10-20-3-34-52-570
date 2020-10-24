6aa1cb5980288ab74adbdb900900b09730e59f99
CANCEAL <alfred.cancejo@oocl.com>
2020-10-24 20:40:14 +0800

    [Employee Controller] [Alfred & Leo] Refactor: Updated toResponse of CompanyMapper

--- src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
@@ -3,17 +3,24 @@ package com.thoughtworks.springbootemployee.mapper;
 import com.thoughtworks.springbootemployee.dto.CompanyRequest;
 import com.thoughtworks.springbootemployee.dto.CompanyResponse;
 import com.thoughtworks.springbootemployee.model.Company;
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import static java.util.Objects.nonNull;
+
+@Aspect
 @Component
 public class CompanyMapper {
+
     public CompanyResponse toResponse(Company company) {
         CompanyResponse companyResponse = new CompanyResponse();
 
         companyResponse.setCompany_id(company.getCompany_id());
         companyResponse.setCompany_name(company.getCompany_name());
-        companyResponse.setEmployee_count(company.getEmployeeCount());
-        companyResponse.setEmployees(company.getEmployeeList());
+
+        boolean companyHasEmployees = nonNull(company.getEmployeeList());
+        companyResponse.setEmployee_count(companyHasEmployees ? company.getEmployeeCount() : 0);
+        companyResponse.setEmployees(companyHasEmployees ? company.getEmployeeList() : null);
 
         return companyResponse;
     }
